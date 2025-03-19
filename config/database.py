ec9d00ba8548f7a7416a7e3f70a6ab28824acda1
Ricardo-Baron <crbar32@gmail.com>
2025-03-19 18:27:45 -0300

    Update database.py

--- config/database.py
+++ config/database.py
@@ -1,6 +1,10 @@
 from pymongo import MongoClient
 
-client=MongoClient("mongodb+srv://crbar32:Ricardo2823*@keyonapp.9u6wm.mongodb.net/?retryWrites=true&w=majority")
+#client=MongoClient("mongodb+srv://crbar32:Ricardo2823*@keyonapp.9u6wm.mongodb.net/?retryWrites=true&w=majority")
+
+MONGO_URL = os.getenv("DATABASE_URL")  # Obtiene la URL de MongoDB desde las variables de entorno
+
+client = MongoClient(MONGO_URL)
 
 db=client.ControlAcceso_db              #->Establecemos el nombre de la base de datos
 collection_name=db["Usuarios"]     #->Establecemos el nombre de la colecion
